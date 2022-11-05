04ab88996e60755b13757a4bff9b0ac759e96b6e
Hyeonsu <gustn05233@naver.com>
2022-11-05 14:43:12 +0900

    계정 DTO 클래스 생성: 계정 DB 데이터 저장 [싱글턴 패턴]

--- src/deu/cse/team/singleton/example.java
+++ src/deu/cse/team/singleton/example.java
@@ -6,9 +6,9 @@ package deu.cse.team.singleton;
 
 import java.util.List;
 
-/**
- *
- * @author PC
+/*
+* 2022.11.05 [최초작성자 20183215 정현수]
+* 삽입 연습
  */
 public class example {
 
@@ -24,17 +24,15 @@ public class example {
         dto.setPower("O");
         dto.setAllowed(false);
         boolean r = dao.InsertAccount(dto);
-        ​
+        
         if (r) {
             System.out.println("삽입에 성공했습니다.");
         } else {
             System.out.println("삽입에 실패했습니다.");
         }
-        ​
-
-​
+        
+        
         List<AccountDTO> list = dao.getList();
-
         //list의 모든 데이터에 접근해서 출력
         for (AccountDTO account : list) {
             //출력하는 메서드에 참조형 변수를 사용하면 toString()메서드가 호출되서 출력됩니다.
