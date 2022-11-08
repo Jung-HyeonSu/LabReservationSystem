22af2976787876db6363e6cb5fab6713c9f369a0
Seongchan <Seongchan@DESKTOP-UIN4FBK>
2022-11-08 19:54:43 +0900

    reservation 수정

--- src/deu/cse/team/reservation/beforeReserve.java
+++ src/deu/cse/team/reservation/beforeReserve.java
@@ -10,14 +10,18 @@ import deu.cse.team.singleton.DAO;
 import deu.cse.team.singleton.ReservationDTO;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
  *
  * @author Seongchan
  */
-public class newReserve extends javax.swing.JFrame {
+public class beforeReserve extends javax.swing.JFrame {
 
     /**
         2022.11.07 [최초작성자 20183207 김성찬]
@@ -35,13 +39,14 @@ public class newReserve extends javax.swing.JFrame {
     Reservation reservation = new Reservation();
     ReservationOkCommand reservationOk = new ReservationOkCommand(reservation);
     ReservationCancelCommand reservationCancel = new ReservationCancelCommand(reservation);    
-    DAO dao = DAO.getInstance();  
-    public newReserve() {        
+    DAO dao = DAO.getInstance(); 
+    
+    public beforeReserve() {        
         initComponents();        
         remoteControl.setCommand(1, reservationOk, reservationCancel);
         max=40;//값가져와서 변경하기
         setSeat();
-        responsiblename.setText("이름가져오기");
+        responsiblename.setText("이름가져오기");         
     }
     
     
@@ -87,6 +92,14 @@ public class newReserve extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        editTime = new javax.swing.JDialog();
+        jLabel1 = new javax.swing.JLabel();
+        jlabel = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        changebtn1 = new javax.swing.JButton();
+        cancelbtn = new javax.swing.JButton();
+        starttimebox = new javax.swing.JComboBox<>();
+        endtimebox = new javax.swing.JComboBox<>();
         jLabel31 = new javax.swing.JLabel();
         responsiblename = new javax.swing.JLabel();
         classnumber = new javax.swing.JLabel();
@@ -97,6 +110,78 @@ public class newReserve extends javax.swing.JFrame {
         resertime = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+
+        jLabel1.setFont(new java.awt.Font("맑은 고딕", 1, 24)); // NOI18N
+        jLabel1.setText("시간설정");
+
+        jlabel.setText("시작시간");
+
+        jLabel3.setText("종료시간");
+
+        changebtn1.setText("변경");
+        changebtn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                changebtn1ActionPerformed(evt);
+            }
+        });
+
+        cancelbtn.setText("취소");
+        cancelbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelbtnActionPerformed(evt);
+            }
+        });
+
+        starttimebox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "9", "10", "11", "12", "13", "14", "15", "16", "17" }));
+
+        endtimebox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "10", "11", "12", "13", "14", "15", "16", "17" }));
+
+        javax.swing.GroupLayout editTimeLayout = new javax.swing.GroupLayout(editTime.getContentPane());
+        editTime.getContentPane().setLayout(editTimeLayout);
+        editTimeLayout.setHorizontalGroup(
+            editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(editTimeLayout.createSequentialGroup()
+                .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(editTimeLayout.createSequentialGroup()
+                        .addGap(140, 140, 140)
+                        .addComponent(jLabel1))
+                    .addGroup(editTimeLayout.createSequentialGroup()
+                        .addGap(99, 99, 99)
+                        .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(editTimeLayout.createSequentialGroup()
+                                .addComponent(changebtn1)
+                                .addGap(48, 48, 48)
+                                .addComponent(cancelbtn))
+                            .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(editTimeLayout.createSequentialGroup()
+                                    .addComponent(jLabel3)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(endtimebox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(editTimeLayout.createSequentialGroup()
+                                    .addComponent(jlabel)
+                                    .addGap(56, 56, 56)
+                                    .addComponent(starttimebox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addContainerGap(109, Short.MAX_VALUE))
+        );
+        editTimeLayout.setVerticalGroup(
+            editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(editTimeLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addGap(33, 33, 33)
+                .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlabel)
+                    .addComponent(starttimebox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(43, 43, 43)
+                .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(endtimebox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addGroup(editTimeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(changebtn1)
+                    .addComponent(cancelbtn))
+                .addGap(45, 45, 45))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -199,7 +284,9 @@ public class newReserve extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void changebtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changebtnActionPerformed
-        // TODO add your handling code here:
+        editTime.setVisible(true);
+        editTime.setLocationRelativeTo(this);
+        editTime.setSize(450, 400);
     }//GEN-LAST:event_changebtnActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -211,18 +298,32 @@ public class newReserve extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
-                    if ("ok".equals(remoteControl.A_ButtonWasPushed(1))) {
+        if ("ok".equals(remoteControl.A_ButtonWasPushed(1))) {
             showMessageDialog(null, Message);            
         }
         String time[] = resertime.getText().split("~");
         starttime = time[0].trim();
         endtime = time[1].trim();
         ReservationDTO rdto = new ReservationDTO(dao.getReserLength(),Integer.parseInt(seatnumber),responsiblename.getText(),classnumber.getText(),starttime,endtime);
-        boolean checkReservation = dao.InsertReservation(rdto);
-            
-    
+        boolean checkReservation = dao.InsertReservation(rdto);  
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void changebtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changebtn1ActionPerformed
+        // TODO add your handling code here:
+        starttime = starttimebox.getSelectedItem().toString();
+        endtime = endtimebox.getSelectedItem().toString();        
+        if (Integer.parseInt(starttime) > Integer.parseInt(endtime)) showMessageDialog(null, "시작 시간이 종료 시간보다 클 수는 없습니다.");
+        else if (Integer.parseInt(starttime) == Integer.parseInt(endtime)) showMessageDialog(null, "시작 시간이 종료 시간과 같을 수는 없습니다.");
+        else{
+            editTime.dispose();
+            resertime.setText(starttime+":00 ~ "+endtime+":00");
+        }  
+    }//GEN-LAST:event_changebtn1ActionPerformed
+
+    private void cancelbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelbtnActionPerformed
+        // TODO add your handling code here:
+        editTime.dispose();
+    }//GEN-LAST:event_cancelbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -241,36 +342,46 @@ public class newReserve extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(newReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(beforeReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(newReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(beforeReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(newReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(beforeReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(newReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(beforeReserve.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new newReserve().setVisible(true);
+                new beforeReserve().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cancelbtn;
     private javax.swing.JButton changebtn;
+    private javax.swing.JButton changebtn1;
     private javax.swing.JLabel classnumber;
+    private javax.swing.JDialog editTime;
+    private javax.swing.JComboBox<String> endtimebox;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel31;
     private javax.swing.JLabel jLabel35;
-    private javax.swing.JLabel resertime;
+    private javax.swing.JLabel jlabel;
+    public javax.swing.JLabel resertime;
     private javax.swing.JLabel resertimearea;
     private javax.swing.JLabel responsiblename;
     private javax.swing.JLabel settotal;
+    private javax.swing.JComboBox<String> starttimebox;
     // End of variables declaration//GEN-END:variables
 }
 
