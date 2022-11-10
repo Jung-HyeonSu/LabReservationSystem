d0c395cb8dca572c52cfa22be7fa691a39da265f
Hyeonsu <gustn05233@naver.com>
2022-11-10 21:04:24 +0900

    토큰값 인증 여부 업데이트 메서드 추가

--- src/deu/cse/team/singleton/DAO.java
+++ src/deu/cse/team/singleton/DAO.java
@@ -537,6 +537,37 @@ public class DAO {
         return result;
     }
     
+    public boolean UpdateAccount(AccountDTO account, String id, String allowed) {
+        boolean result = false;
+
+        if (this.connect()) {
+            try {
+                String sql = "UPDATE account SET allowed =(?) WHERE id =" + id;
+
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, allowed);
+                
+                int r = pstmt.executeUpdate();
+
+                if (r > 0) {
+                    result = true;
+                }
+                //데이터베이스 생성 객체 해제
+                pstmt.close();
+                this.close();
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        } else {
+            System.out.println("데이터베이스 연결에 실패");
+            System.exit(0);
+        }
+
+        return result;
+    }
+    
     public boolean UpdateAccount(AccountDTO account, String id, String password, String name, String phonenumber) {
         boolean result = false;
 
